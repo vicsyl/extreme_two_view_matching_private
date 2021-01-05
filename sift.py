8ad21621a9bc9ae6fd28207950d5bf8168e2f0a8
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2021-01-05 01:01:10 +0100

    local features, ransac and homography estimation

--- sift.py
+++ sift.py
@@ -1,23 +1,56 @@
-import numpy as np
-import cv2
 from scene_info import *
+import cv2 as cv
 import matplotlib.pyplot as plt
-# from copy import deepcopy
-from ransac import *
+import numpy as np
 
-#TODO from verify
+"""
+    DISCLAIMER: some of the these methods were taken from templates (.py files or jupyter notebooks) 
+    for the tasks for the MPV course taken by me (Vaclav Vavra) in the spring semester 2020.
+"""
+
+
+def decolorize(img):
+    return cv.cvtColor(cv.cvtColor(img,cv.COLOR_RGB2GRAY), cv.COLOR_GRAY2RGB)
+
+
+def draw_matches(kps1, kps2, tentative_matches, H, inlier_mask, img1, img2):
+    h, w, ch = img1.shape
+    pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+    dst = cv.perspectiveTransform(pts, H)
+    img2_tr = cv.polylines(decolorize(img2), [np.int32(dst)], True, (0, 0, 255), 3, cv.LINE_AA)
+    matches_mask = inlier_mask.ravel().tolist()
+
+    # Blue is estimated homography
+    draw_params = dict(matchColor = (255, 255, 0), # draw matches in yellow color
+                   singlePointColor = None,
+                   matchesMask = matches_mask, # draw only inliers
+                   flags = 20)
+    img_out = cv.drawMatches(decolorize(img1), kps1, img2_tr, kps2, tentative_matches, None, **draw_params)
+    plt.figure(figsize=(20, 10))
+    plt.title("Matches with estimated homography")
+    plt.imshow(img_out)
+    plt.show()
+    return
+
+
 def split_points(tentative_matches, kps1, kps2):
     src_pts = np.float32([ kps1[m[0].queryIdx].pt for m in tentative_matches ]).reshape(-1,2)
     dst_pts = np.float32([ kps2[m[0].trainIdx].pt for m in tentative_matches ]).reshape(-1,2)
     return src_pts, dst_pts
 
+# TODO split this into ransac and matching
+def match_by_ransac(descriptor, img1, img2, ratio_thresh=0.8, show=True):
 
-def alternative_method(img1, img2, ratio_thresh=0.8, show=True):
+    kps1, descs1 = descriptor.detectAndCompute(img1, None)
+    kps2, descs2 = descriptor.detectAndCompute(img2, None)
 
-    det = cv2.AKAZE_create()
-    kps1, descs1 = det.detectAndCompute(img1, None)
-    kps2, descs2 = det.detectAndCompute(img2, None)
-    matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE_HAMMING)
+    # img_sift_keypoints1 = cv.drawKeypoints(img1, kps1, img1, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # img_sift_keypoints2 = cv.drawKeypoints(img2, kps2, img2, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    #
+    # cv.imwrite('work/sift_keypoints_{}.jpg'.format(img_pair.img1), img_sift_keypoints1)
+    # cv.imwrite('work/sift_keypoints_{}.jpg'.format(img_pair.img2), img_sift_keypoints2)
+
+    matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_BRUTEFORCE_HAMMING)
     knn_matches = matcher.knnMatch(descs1, descs2, 2)
 
     tentative_matches_structured = []
@@ -28,71 +61,28 @@ def alternative_method(img1, img2, ratio_thresh=0.8, show=True):
             tentative_matches_flat.append(m)
 
     if show:
-        img3 = cv2.drawMatchesKnn(img1, kps1, img2, kps2, tentative_matches_structured, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        plt.imshow(img3), plt.show()
+        img3 = cv.drawMatchesKnn(img1, kps1, img2, kps2, tentative_matches_structured, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        plt.imshow(img3)
+        plt.show()
 
 
     src_pts, dst_pts = split_points(tentative_matches_structured, kps1, kps2)
-    H, inlier_mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 1.0)
+    H, inlier_mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 1.0)
 
-    # draw_matches(kps1, kps2, tentative_matches,
-    #              H.detach().cpu().numpy(),
-    #              H_gt,
-    #              inl.cpu().numpy(),
-    #              cv2.cvtColor(img1, cv2.COLOR_GRAY2RGB),
-    #              cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB))
-
-    draw_matches(kps1, kps2, tentative_matches_flat,
-             H,
-             H,
-             inlier_mask,
-             img1,
-             img2)
-             # cv2.cvtColor(img1, cv2.COLOR_GRAY2RGB),
-             # cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB))
-
-
-def sift_1(img1, img2, img_pair):
-
-    sift = cv2.SIFT_create()
-
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
-
-    img_sift_keypoints1 = cv2.drawKeypoints(img1, kp1, img1, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    img_sift_keypoints2 = cv2.drawKeypoints(img2, kp2, img2, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    cv2.imwrite('work/sift_keypoints_{}.jpg'.format(img_pair.img1), img_sift_keypoints1)
-    cv2.imwrite('work/sift_keypoints_{}.jpg'.format(img_pair.img2), img_sift_keypoints2)
-
-    # TODO
-
-    # BFMatcher with default params
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
-
-    # Apply ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append([m])
-
-    # cv2.drawMatchesKnn expects list of lists as matches.
-    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.imshow(img3)
-    plt.show()
-
+    draw_matches(kps1, kps2, tentative_matches_flat, H, inlier_mask, img1, img2)
 
 
 if __name__ == "__main__":
+
+    akaze_desc = cv.AKAZE_create()
+    sift = cv.SIFT_create()
 
     img_pairs = read_image_pairs("scene1")
 
     img_pair: ImagePairEntry = img_pairs[0][0]
 
-    img1 = cv2.imread('original_dataset/scene1/images/{}.jpg'.format(img_pair.img1))
-    img2 = cv2.imread('original_dataset/scene1/images/{}.jpg'.format(img_pair.img2))
+    img1 = cv.imread('original_dataset/scene1/images/{}.jpg'.format(img_pair.img1))
+    img2 = cv.imread('original_dataset/scene1/images/{}.jpg'.format(img_pair.img2))
 
-    alternative_method(img1, img2, ratio_thresh=0.75, show=False)
-    #sift_1(img1, img2, img_pair)
+    match_by_ransac(akaze_desc, img1, img2, ratio_thresh=0.75, show=True)
     print("done")
