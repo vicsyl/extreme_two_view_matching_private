fix in computation of normals

depth_to_normals.py
@@ -219,15 +219,13 @@ def get_rotation_matrices_across_img(camera: CameraEntry):
 
     f_norms = np.expand_dims(np.linalg.norm(d, axis=2), axis=2)
     d_unit = d / f_norms
-    d_unit_t = np.moveaxis(d_unit, -1, -2)
+
+    # OK - checked
+    d_unit_row_vec = np.moveaxis(d_unit, -1, -2)
 
     z = np.array([0.0, 0.0, 1.0])
 
-    rotation_vector2 = np.cross(z, d_unit_t)
-    rotation_vector = np.cross(d_unit_t, z)
-
-    #rotation_vector2 = np.cross(np.squeeze(d_unit), np.squeeze(z))
-    #diff = np.squeeze(rotation_vector) - rotation_vector2
+    rotation_vector = np.cross(z, d_unit_row_vec)
 
     rotation_vector_norm = np.linalg.norm(rotation_vector, axis=3)
     sin_theta = np.linalg.norm(rotation_vector, axis=3)
@@ -239,14 +237,7 @@ def get_rotation_matrices_across_img(camera: CameraEntry):
     Rs = get_rotation_matrices(unit_rotation_vector, theta)
     det = np.linalg.det(Rs)
     assert np.all(np.abs(det - 1.0) < 0.0001)
-    check = Rs @ d_unit
-    check = np.squeeze(check)
-    check2 = Rs @ z
-    check3 = Rs @ -d_unit
-    check3 = np.squeeze(check3)
-
-    # check should be (w, h, [0, 0, 1], 0)
-
+    # Rs[w, h], (x, y) coord OK
     return Rs
 
 
@@ -262,41 +253,25 @@ def diff_normal_from_depth_data(camera: CameraEntry,
 
     Rs = get_rotation_matrices_across_img(camera)
     Rs = torch.from_numpy(Rs)
+    # (x, y) -> (y, x)
+    Rs = Rs.permute((1, 0, 2, 3))
 
     # Could be also done from reprojected data, but this seems to be correct and more straightforward
-    gradient_dzdx_o, gradient_dzdy_o = spatial_gradient_first_order(depth_data, mask=mask, smoothed=smoothed, sigma=sigma)
-
     gradient_dzdx, gradient_dzdy = spatial_gradient_first_order(depth_data, mask=mask, smoothed=smoothed, sigma=sigma)
     gradient_dzdx = (gradient_dzdx * focal_length / depth_data).unsqueeze(dim=4)
     gradient_dzdy = (gradient_dzdy * focal_length / depth_data).unsqueeze(dim=4)
     z_ones = torch.ones(gradient_dzdy.shape)
-    #z_ones = torch.zeros(gradient_dzdy.shape)
     normals = torch.cat((-gradient_dzdx, -gradient_dzdy, -z_ones), dim=4)
     normals_norms = torch.norm(normals, dim=4).unsqueeze(dim=4)
     normals = normals / normals_norms
     normals = normals.squeeze(dim=0).squeeze(dim=0)
     normals = normals.unsqueeze(3)
 
-    normals2 = torch.cat((gradient_dzdx, gradient_dzdy, -z_ones), dim=4)
-    normals_norms2 = torch.norm(normals2, dim=4).unsqueeze(dim=4)
-    normals2 = normals2 / normals_norms2
-    normals2 = normals2.squeeze(dim=0).squeeze(dim=0)
-    normals2 = normals2.unsqueeze(3)
-
-    diff = normals - normals2
-    normals = normals2
-
-    Rs = Rs.permute((1, 0, 2, 3))
-
-    print("give me")
-    normals_orig = normals
     normals = Rs @ normals
     normals = normals.squeeze()
     normals_norms = torch.norm(normals, dim=2).unsqueeze(dim=2)
     normals = normals / normals_norms
     return normals
-    #return normals_orig.squeeze()
-
 
 
 # TODO centralize the logic around the "factor"
@@ -348,26 +323,26 @@ def cluster_and_save_normals(normals,
     if clusters_map.__contains__(depth_data_file_name[:-4]):
         clusters = clusters_map[depth_data_file_name[:-4]]
     else:
-        clusters = 2
-        print("WARNING: number of clusters unknown for {}, defaulting to 2 ...".format(depth_data_file_name))
+        clusters = 1
+        print("WARNING: number of clusters unknown for {}, defaulting to {} ...".format(depth_data_file_name, clusters))
 
     # TODO just confirm if this happens for monodepth
-    # if len(normals.shape) == 5:
-    #     normals = normals.squeeze(dim=0).squeeze(dim=0)
+    if len(normals.shape) == 5:
+        normals = normals.squeeze(dim=0).squeeze(dim=0)
 
-    img = normals.numpy() * 255
-    img[:, :, 2] = -img[:, :, 2] / 255
-
-    if show:
-        fig = plt.figure()
-        plt.title(title)
-        for index in range(3):
-            # row, columns, index
-            ax = fig.add_subplot(131 + index)
-            ax.imshow(img[:, :, index])
-        plt.show()
-    if save:
-        cv.imwrite("{}.jpg".format(file_name_prefix), img)
+    if show or save:
+        img = normals.numpy() * 255
+        img[:, :, 2] = -img[:, :, 2] / 255
+        if show:
+            fig = plt.figure()
+            plt.title(title)
+            for index in range(3):
+                # row, columns, index
+                ax = fig.add_subplot(131 + index)
+                ax.imshow(img[:, :, index])
+            plt.show()
+        if save:
+            cv.imwrite("{}.jpg".format(file_name_prefix), img)
 
     minus_z_direction = torch.zeros(normals.shape)
     minus_z_direction[:, :, 2] = -1.0
@@ -380,14 +355,14 @@ def cluster_and_save_normals(normals,
     clustered_normals, normal_indices = spherical_kmeans.kmeans(normals, filtered, clusters)
     Timer.end_check_point("clustering normals")
 
-    img[:, :, 0][normal_indices == 0] = 255
-    img[:, :, 0][normal_indices != 0] = 0
-    img[:, :, 1][normal_indices == 1] = 255
-    img[:, :, 1][normal_indices != 1] = 0
-    img[:, :, 2][normal_indices == 2] = 255
-    img[:, :, 2][normal_indices != 2] = 0
-
     if show or save:
+        img[:, :, 0][normal_indices == 0] = 255
+        img[:, :, 0][normal_indices != 0] = 0
+        img[:, :, 1][normal_indices == 1] = 255
+        img[:, :, 1][normal_indices != 1] = 0
+        img[:, :, 2][normal_indices == 2] = 255
+        img[:, :, 2][normal_indices != 2] = 0
+
         #plt.figure()
         color_names = ["red", "green", "blue"]
         desc = ""
@@ -452,7 +427,15 @@ def compute_normals_simple_diff_convolution(scene: SceneInfo, depth_data_read_di
     return clustered_normals_np, normal_indices_np
 
 
-def compute_normals_simple_diff_convolution_simple(camera, depth_data_read_directory, depth_data_file_name, save, output_directory, upsample=False):
+def compute_normals_simple_diff_convolution_simple(
+        camera,
+        depth_data_read_directory,
+        depth_data_file_name,
+        save,
+        output_directory,
+        upsample=False,
+        override_mask=None,
+        old_implementation=False):
     """see :func:`compute_normals_simple_diff_convolution`"""
 
     height = camera.height_width[0]
@@ -495,16 +478,10 @@ def compute_normals_simple_diff_convolution_simple(camera, depth_data_read_direc
         # for depth_factor_inv in range(6, 7, 2):
         #     normals_params_list.append((True, sigma, "sigma_5_{}".format(1/depth_factor_inv), 1/depth_factor_inv))
 
+    if override_mask is not None:
+        mask = override_mask
 
-    mask = torch.tensor([[0.5, 0.5, 0.5, 0.5, 0.5,
-                          0.5, 0.5, 0.5, 0.5, 0.5,
-                          0,
-                         -0.5, -0.5, -0.5, -0.5, -0.5,
-                         -0.5, -0.5, -0.5, -0.5, -0.5,
-                         ]]).float() / 10
-    #mask = torch.tensor([[0.5, 0.5, 0, -0.5, -0.5]]).float() / 2
-    sigma = 5
-    smoothed = True
+    smoothed = False
 
     if not upsample:
         width = None
@@ -512,7 +489,10 @@ def compute_normals_simple_diff_convolution_simple(camera, depth_data_read_direc
 
     depth_data = read_depth_data(depth_data_file_name, depth_data_read_directory, height, width)
 
-    normals = diff_normal_from_depth_data(camera, depth_data, mask=mask, smoothed=smoothed, sigma=sigma)
+    if old_implementation:
+        normals = diff_normal_from_depth_data_old(camera.focal_length, depth_data, mask=mask, smoothed=smoothed, sigma=sigma)
+    else:
+        normals = diff_normal_from_depth_data(camera, depth_data, mask=mask, smoothed=smoothed, sigma=sigma)
     print("Creating dir (if not exists already): {}".format(output_directory))
     Path(output_directory).mkdir(parents=True, exist_ok=True)
 

depth_to_normals_tests.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 import cv2 as cv
 
@@ -5,6 +6,7 @@ from scene_info import read_cameras, CameraEntry
 from dataclasses import dataclass
 from depth_to_normals import compute_normals_simple_diff_convolution_simple
 
+from img_utils import show_normals_components
 
 @dataclass
 class DepthSyntheticData:
@@ -57,6 +59,8 @@ def depth_map_of_plane(dsd: DepthSyntheticData, allow_and_nullify_negative_depth
     if save:
         np.save("{}/{}".format(dsd.file_dir_and_name[0], dsd.file_dir_and_name[1]), depth)
 
+    return depth
+
 
 def get_file_dir_and_name(plane):
     #plane = plane[:3]
@@ -64,57 +68,71 @@ def get_file_dir_and_name(plane):
     return "work/tests/", "depth_map_{}.npy".format(file_name_fuffix)
 
 
-def get_depth_synthetic_data():
+def test_depth_to_normals(old_implementation=True):
 
-    planes = np.array([
+    planes_coeffs = np.array([
         [0, 0, 1, -1],
-        # [0, 0, 1, -100],
-        # [1, 0, 1, -1],
-        # [1, 0, 1, -100000],
-        # [0, 1, 1, -1],
-        # [1, 2, 2, -1],
-        # [1, 3, 3, -1],
-        # [1, 1, 1, -1],
-        # [2, 1, 2, -1],
-        # [3, 1, 3, -1],
+        [0, 0, 1, -100],
+        [1, 0, 1, -1],
+        [1, 0, 1, -100000],
+        [0, 1, 1, -1],
+        [1, 2, 2, -1],
+        [1, 3, 3, -1],
+        [1, 1, 1, -1],
+        [2, 1, 2, -1],
+        [3, 1, 3, -1],
     ])
 
     cameras = read_cameras("scene1")
     first_camera = next(iter(cameras.values()))
 
-    return [DepthSyntheticData(plane=plane,
-                               camera=first_camera,
-                               file_dir_and_name=get_file_dir_and_name(plane))
-            for plane in planes]
+    for plane in planes_coeffs:
+
+        #plane = -plane
+
+        exact = plane[:3].copy()
+        exact = -exact / np.linalg.norm(exact)
+        print("\n\n\n\nTesting synthetic depth map for plane coeffs (ax + by + cz + d = 0): {}".format(plane))
+
+        dsd = DepthSyntheticData(plane=plane, camera=first_camera, file_dir_and_name=get_file_dir_and_name(plane))
+        depth_map_of_plane(dsd, allow_and_nullify_negative_depths=False, save=True)
+
+        mask = torch.tensor([[0.5, 0, -0.5]]).float()
+        depth, normals, clustered_normals, normal_indices = \
+            compute_normals_simple_diff_convolution_simple(dsd.camera,
+                                                           dsd.file_dir_and_name[0],
+                                                           dsd.file_dir_and_name[1],
+                                                           save=True,
+                                                           output_directory=dsd.file_dir_and_name[0],
+                                                           override_mask=mask,
+                                                           old_implementation=old_implementation
+                                                           )
+        normals_diff = normals - dsd.plane[:3]
+        show_normals_components(normals_diff, "difference from exact result", (30.0, 20.0))
+
+        if len(normals.shape) == 5:
+            normals = normals.squeeze(dim=0).squeeze(dim=0)
+
+        normals_np = normals.numpy()[5:-5, 5:-5]
+        diff = normals_np - exact
 
 
-def generate_depth_info():
-    return [depth_map_of_plane(dsd, False) for dsd in get_depth_synthetic_data()]
+        maxima = np.max(normals_np, axis=(0, 1))
+        minima = np.min(normals_np, axis=(0, 1))
+        max_dev = maxima - exact
+        min_dev = minima - exact
 
+        print("exact normal of the plane (x, y, y): {}".format(exact))
+        print("normal component maxima (x, y, y): {}".format(maxima))
+        print("normal component minima (x, y, y): {}".format(minima))
+        print("difference of normal component maxima from the exact values (x, y, y): {}".format(max_dev))
+        print("difference of normal component minima from the exact values (x, y, y): {}".format(min_dev))
 
-def test_depth_to_normals():
-
-    data = None
-    data_old = None
-    for dsd in get_depth_synthetic_data():
-        depth_map_of_plane(dsd, False)
-
-        if data is not None:
-            data_old = data
-        data = compute_normals_simple_diff_convolution_simple(dsd.camera, dsd.file_dir_and_name[0], dsd.file_dir_and_name[1], save=True, output_directory=dsd.file_dir_and_name[0])
-        depth, normals, clustered_normals, normal_indices = data
-
-        if data_old is not None:
-            depth_old, normals_old, _, _ = data_old
-            diff_normals = normals - normals_old
-            diff_depth = depth * 100 - depth_old
-
-            print()
-        print()
         # assert np.all(normal_indices == 0.0)
         # assert normals.shape[0] == 1
         # assert np.equal(normals[0], dsd.plane[:3])
 
 
 if __name__ == "__main__":
-    test_depth_to_normals()
+    test_depth_to_normals(old_implementation=False)
+    #test_depth_to_normals(old_implementation=False)