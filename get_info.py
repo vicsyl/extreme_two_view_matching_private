fdf80944bc626deccc9ff785125ec0f3307ed87e
Vaclav Vavra <mr.vaclav.vavra@gmail.com>
2021-11-03 18:02:31 +0100

    bugfix

--- get_info.py
+++ get_info.py
@@ -1,13 +1,11 @@
 import argparse
-from config import Config
-from sky_filter import *
 from pipeline import Pipeline
 
 
 def main():
     parser = argparse.ArgumentParser(prog='pipeline')
     args = parser.parse_args()
-    pipeline = Pipeline.configure("config.txt", args)
+    pipeline, _ = Pipeline.configure("config.txt", args)
     print(pipeline.output_dir)
 
 
